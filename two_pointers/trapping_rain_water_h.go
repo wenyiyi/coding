0f03d84eca325d9eff349eec5ed9d66ba967a2e6
noelwen <wenyimei@126.com>
2026-08-21 20:49:39 +0800

    Add difficulty type label

---
+++ two_pointers/trapping_rain_water_h.go
@@ -0,0 +1,14 @@
+package two_pointers
+
+/*
+	DESCRIPTION (inspired by Leetcode.com)
+	Write a function to calculate the total amount of water trapped between bars on an elevation map,
+	where each bar's width is 1. The input is given as an array of n non-negative integers height representing the height of each bar.
+
+	Input: height = [3, 4, 1, 2, 2, 5, 1, 0, 2]
+	Output: 10 (3+2+2+1+2)
+*/
+
+func trappingWater(height []int) int {
+	return 0
+}
