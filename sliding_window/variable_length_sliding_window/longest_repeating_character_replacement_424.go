62a40dc72a22bad863df638bcc3d56e7fb97df92
noel <wenyimei@126.com>
2026-09-17 14:44:01 +0800

    variable slide window

--- sliding_window/variable_length_sliding_window/longest_repeating_character_replacement_424.go
+++ sliding_window/variable_length_sliding_window/longest_repeating_character_replacement_424.go
@@ -17,26 +17,55 @@ The longest substring with identical letters is "BBBBB", which has a length of 5
 */
 
 /*
-思路：计算符合条件的最大窗口的长度，符合条件的最大窗口的长度 - 窗口内相同字符的个数 = k
-Solution: Find the longest valid window
-A valid window: window length - the count of the most frequent character <= k
+# A A B B A
+A → 3  ← 最多
+B → 2
+window length = 5
+max frequency = 3
+需要替换 = 5 - 3 = 2
+
+windowSize - maxFrequenty <= k
+
+窗口用start和end来表示，窗口长度=end-start+1
+
+移动窗口
+如果合法：
+windowSize - maxFrequency <= k
+→ 继续扩大窗口
+
+如果非法：
+windowSize - maxFrequency > k
+→ frequency[s[left]]--
+→ left++
+
+核心逻辑：
+ 1. right 不断向右
+ 2. 统计当前字符 frequency
+ 3. 更新 maxFrequency
+ 4. windowSize - maxFrequency > k
+    → shrink left
+ 5. 记录最大 windowSize
 */
 func characterReplacement(s string, k int) int {
+	var result int
+	// var window = make([]byte, k) 不需要创建窗口，窗口本身由 start 和 end 表示了，窗口的长度 = end-start+1
+	var windowMap = make(map[byte]int)
+	var maxFrequency int
+
 	start := 0
-	countMap := make(map[byte]int)
-	maxSameCount := 0 // The maximum number of the same character in the current window
-	result := 0
+	for end := 0; end < len(s); end++ {
+		windowMap[s[end]]++
+		if windowMap[s[end]] > maxFrequency {
+			maxFrequency = windowMap[s[end]]
+		}
 
-	for end := range s {
-		countMap[s[end]]++
-		maxSameCount = max(maxSameCount, countMap[s[end]])
-
-		// replacements needed = window length - most same character count
-		if end-start+1-maxSameCount > k {
-			countMap[s[start]]--
+		// 非法      windowSize - maxFrequency <= k     windowSize=end-start
+		if end-start+1-maxFrequency > k {
+			windowMap[s[start]]--
 			start++
 		}
 
+		// result 应该表示历史上出现过的最大合法窗口，而不是“当前窗口长度”
 		result = max(result, end-start+1)
 	}
 
