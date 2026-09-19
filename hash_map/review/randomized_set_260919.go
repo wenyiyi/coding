3f6d591ae0d550ba485c76d29614bd8a2caa3f76
noel <wenyimei@126.com>
2026-09-19 15:49:11 +0800

    review

---
+++ hash_map/review/randomized_set_260919.go
@@ -0,0 +1,83 @@
+package review
+
+import "math/rand"
+
+/*
+实现 RandomizedSet 类：
+
+Insert(val int) bool
+
+向集合中插入 val。
+
+如果 val 不存在，插入并返回 true
+如果已经存在，返回 false
+Remove(val int) bool
+
+从集合中删除 val。
+
+如果 val 存在，删除并返回 true
+如果不存在，返回 false
+GetRandom() int
+
+随机返回集合中的一个元素。
+
+每个元素被选中的概率相同
+可以假设调用时集合中至少有一个元素
+
+*/
+
+type RandomizedSet struct {
+	// map insert/remove
+	// array getRandom
+	indexMap map[int]int
+	values   []int
+}
+
+func Constructor() RandomizedSet {
+	// todo 搞懂各种初始化
+	// m1 := map[int]int{}       → 空 map，已初始化 ✅
+	// m2 := make(map[int]int)   → 空 map，已初始化 ✅
+	// var m3 map[int]int        → nil map，未初始化 ⚠️
+
+	// []int{}            → []           len=0 → append
+	// make([]int, 0)     → []           len=0 → append
+	// make([]int, 3)     → [0, 0, 0]    len=3 → 可以 arr[i] =
+	// var a []int        → nil slice    len=0 → 也可以 append
+	return RandomizedSet{
+		indexMap: map[int]int{},
+		values:   []int{},
+	}
+}
+
+func (this *RandomizedSet) Insert(val int) bool {
+	_, ok := this.indexMap[val]
+	if ok {
+		return false
+	}
+	this.values = append(this.values, val)
+	this.indexMap[val] = len(this.values) - 1
+	return true
+}
+
+func (this *RandomizedSet) Remove(val int) bool {
+	// 最后一个数填到要删除的数的位置
+	index, ok := this.indexMap[val]
+	if !ok {
+		return false
+	}
+	lastIndex := len(this.values) - 1
+	lastVal := this.values[lastIndex]
+
+	this.values[index] = lastVal
+	this.indexMap[lastVal] = index
+
+	// 删除元素
+	this.values = this.values[:lastIndex]
+	// todo map 和 数组都要删
+	delete(this.indexMap, lastVal)
+	return true
+}
+
+func (this *RandomizedSet) GetRandom() int {
+	return this.values[rand.Intn(len(this.values))]
+}
---
+++ two_pointers/review/3_sum_260919.go
@@ -0,0 +1,73 @@
+package review
+
+import "sort"
+
+/*
+给定整数数组 nums，返回所有满足下面条件且不重复的三元组：
+
+nums[i] + nums[j] + nums[k] == 0
+
+要求：
+i、j、k 两两不同
+答案中不能出现重复的三元组
+
+Example 1
+Input:
+nums = [-1, 0, 1, 2, -1, -4]
+Output:
+[[-1, -1, 2], [-1, 0, 1]]
+*/
+
+/*
+排序
+固定 一个数(也不能重复)
+找两外两个数，left right
+
+重复的数就跳过
+
+*/
+
+func threeSum(nums []int) [][]int {
+	result := [][]int{}
+
+	// todo 排序
+	sort.Ints(nums)
+
+	// -1, 0, 1  i=0  len=3 i<3-2=1
+	// todo 应该是 i < len(nums)-2 而不是 i < len(nums)，固定 i，后面还需要 left + right 两个元素，i 后面至少留 2 个
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		// todo left从i+1开始，不是0
+		left, right := i+1, len(nums)-1
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
+			if sum == 0 {
+				result = append(result, []int{nums[i], nums[left], nums[right]})
+				// todo 先移动，再跟刚才使用过的值对比
+				// for left < right && nums[left] == nums[left+1] {
+				//					left++
+				//				}
+				//				for left < right && nums[right] == nums[right-1] {
+				//					right--
+				//				}
+				left++
+				right--
+				for left < right && nums[left] == nums[left-1] {
+					left++
+				}
+				for left < right && nums[right] == nums[right+1] {
+					right--
+				}
+			}
+			if sum > 0 {
+				right--
+			}
+			if sum < 0 {
+				left++
+			}
+		}
+	}
+	return result
+}
