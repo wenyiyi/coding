efc6774bacf9cf87ad3befb31c460071fd98f340
noel <wenyimei@126.com>
2026-09-19 14:25:49 +0800

    rotate matrix

--- matrix/rotate_image_48.go
+++ matrix/rotate_image_48.go
@@ -2,12 +2,154 @@ package matrix
 
 /*
 https://leetcode.com/problems/rotate-image/description/
-（卓驭一面面试题）
+You are given an n x n 2D matrix representing an image,
+rotate the image by 90 degrees (clockwise顺时针).
 
+You have to rotate the image in-place原地旋转,
+which means you have to modify the input 2D matrix directly.
+DO NOT allocate分配 another 2D matrix and do the rotation.
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+  0 1 2
+0 1 2 3
+1 4 5 6
+2 7 8 9
+
+   0 1 2
+ 0 7 4 1
+ 1 8 5 2
+ 2 9 6 3
+
+Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+
+  0  1  2  3
+0 5  1  9  11
+1 2  4  8  10
+2 13 3  6  7
+3 15 14 12 16
+
+  0   1   2  3
+0 15  13  2  5
+1 14  3   4  1
+2 12  6   8  9
+3 16  7  10 11
 
 */
 
 /*
+	0  1  2  3
 
+0 5  1  9  11
+1 2  4  8  10
+2 13 3  6  7
+3 15 14 12 16
 
- */
+第一步：Transpose
+
+	0  1  2  3
+
+0 5  1  9  11
+1 -  4  8  10
+2 -  -  6  7
+3 -  -  -  16
+
+第二步：每一行左右反转
+
+	  0 1 2
+	0 7 4 1
+	1 8 5 2
+	2 9 6 3
+*/
+func rotate(matrix [][]int) {
+
+	// 第1步，先转置
+	for i := 0; i < len(matrix); i++ {
+		for j := i + 1; j < len(matrix[i]); j++ {
+			matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+		}
+	}
+
+	// 第2步，每一行左右反转
+	for i := 0; i < len(matrix); i++ {
+		left := 0
+		right := len(matrix[i]) - 1
+
+		// todo 不止是两个边角元素需要反转
+		for left < right {
+			matrix[i][right], matrix[i][left] = matrix[i][left], matrix[i][right]
+			left++
+			right--
+		}
+	}
+
+}
+
+/*
+  0 1 2
+0 1 2 3
+1 4 5 6
+2 7 8 9
+
+   0 1 2
+ 0 7 4 1
+ 1 8 5 2
+ 2 9 6 3
+
+top  col=right row=top++
+1 (0,0)-(0,2)
+2 (0,1)-(1,2)
+3 (0,2)-(2,2)
+
+right
+6 (1,2)-(2,1)
+9 (2,2)-(2,0)
+
+bottom
+8 (2,1)-(1,0)
+7 (2,0)-(0,0)
+
+left
+4 (1,0)-(0,1)
+
+先处理外圈，再处理内圈
+
+这个版本坐标太绕了，先不记这个版本
+*/
+
+func rotate2(matrix [][]int) {
+	top := 0
+	left := 0
+	right := len(matrix[0]) - 1
+	bottom := len(matrix) - 1
+
+	for left < right && top < bottom {
+		for col := left; col < right; col++ {
+			offset := col - left
+
+			temp := matrix[top][left+offset]
+
+			// top -> right
+			matrix[top+offset][right], temp =
+				temp, matrix[top+offset][right]
+
+			// right -> bottom
+			matrix[bottom][right-offset], temp =
+				temp, matrix[bottom][right-offset]
+
+			// bottom -> left
+			matrix[bottom-offset][left], temp =
+				temp, matrix[bottom-offset][left]
+
+			// left -> top
+			matrix[top][left+offset] = temp
+		}
+
+		top++
+		bottom--
+		left++
+		right--
+	}
+}
--- matrix/spiral_matrix_54.go
+++ matrix/spiral_matrix_54.go
@@ -2,4 +2,79 @@ package matrix
 
 /*
 	https://leetcode.com/problems/spiral-matrix/
+   （卓驭一面面试题）
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+
+1 2 3
+4 5 6
+7 8 9
+
+输出 1 ->2-> 3-> 6-> 9-> 8-> 7-> 4-> 5
+
+
+Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+
+1 2  3  4
+5 6  7  8
+9 10 11 12
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 10
+-100 <= matrix[i][j] <= 100
 */
+
+/*
+一圈一圈向内输出矩阵：
+先处理外圈
+再处理内圈
+*/
+func spiralOrder(matrix [][]int) []int {
+	result := []int{}
+
+	left := 0
+	top := 0
+	right := len(matrix[0]) - 1
+	bottom := len(matrix) - 1
+
+	// 单行单列  1 2 3     left=0 right=2 top=0 bottom=0
+	for left <= right && top <= bottom {
+		// todo top(包括left和right两个角) row 不变 col++
+		for col := left; col <= right; col++ {
+			result = append(result, matrix[top][col])
+		}
+
+		// todo right（不包含第一行的右上角，包含最后一行的右下角） col不变 row++
+		for row := top + 1; row <= bottom; row++ {
+			result = append(result, matrix[row][right])
+		}
+
+		// todo 单行的话，bottom=top，不能重复输出
+		if top != bottom {
+			// todo bottom（不包含最右的角，包含最左的角） row不变，col--
+			for col := right - 1; col >= left; col-- {
+				result = append(result, matrix[bottom][col])
+			}
+		}
+
+		// todo 单列的话，left=right
+		if left != right {
+			// todo left（不包含上下两个角） col不变，row++
+			for row := bottom - 1; row > top; row-- {
+				result = append(result, matrix[row][left])
+			}
+		}
+
+		// todo 等外圈处理完再统一缩圈
+		right--
+		top++
+		bottom--
+		left++
+	}
+	return result
+}
